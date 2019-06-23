2f064b76eb39a312c05f3ae1bc25a5a514c3423c
Divyanshu Bhaik <45871524+dv-123@users.noreply.github.com>
2019-06-23 20:08:53 +0530

    Create 2_computeCost.m

---
+++ Assignments_Coursera/Assignment_1/2_computeCost.m
@@ -0,0 +1,11 @@
+function J = computeCost(X, y, theta)
+
+m = length(y);                                      % the number of training examples
+
+J = 0;                                              % initialing the cost
+
+predictions = X*theta;                              % craeting the predicted data with initailised random theeta
+sqrErrors = (predictions-y).^2;                     % computing the squared error
+J = 1/(2*m) * sum(sqrErrors);                       % computing the cost for current theeta
+
+end
